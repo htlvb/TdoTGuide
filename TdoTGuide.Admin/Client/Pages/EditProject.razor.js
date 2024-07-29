da9a3d0d787abf5d2aabf94f5c55f329bf27bc7c
Johannes Egger <j.egger@posteo.at>
2024-07-29 22:39:07 +0200

    Upload files in parallel to prevent expiring upload URLs

--- TdoTGuide.Admin/Client/Pages/EditProject.razor.js
+++ TdoTGuide.Admin/Client/Pages/EditProject.razor.js
@@ -1,19 +1,23 @@
-﻿export async function uploadMedia(/** @type {String[]} */ uploadUrls) {
-    let errors = []
-    /** @type {HTMLInputElement} */ const uploadFileElement = document.querySelector("#new-media")
-    for (let i = 0; i < uploadUrls.length; i++) {
-        const file = uploadFileElement.files[i]
-        try {
-            const response = await fetch(uploadUrls[i], { "method": "PUT", "body": file, "headers": { "Content-Type": file.type } })
-            if (!response.ok) {
-                throw response
-            }
-        }
-        catch (e) {
-            console.error(`Error while uploading ${file.name}`, e)
-            errors.push(`${file.name} konnte nicht hochgeladen werden.`)
-        }
+﻿async function uploadMediaItem(/** @type File */ file, /** @type String */ uploadUrl) {
+    const response = await fetch(uploadUrl, { "method": "PUT", "body": file, "headers": { "Content-Type": file.type } })
+    if (!response.ok) {
+        throw response
     }
+}
+
+export async function uploadMedia(/** @type {String[]} */ uploadUrls) {
+    /** @type {HTMLInputElement} */ const uploadFileElement = document.querySelector("#new-media")
+    let errors = []
+    let promises = []
+    for (let i = 0; i < uploadUrls.length; i++) {
+        const uploadPromise = uploadMediaItem(uploadFileElement.files[i], uploadUrls[i])
+            .catch(error => {
+                console.error(`Error while uploading ${file.name}`, e)
+                errors.push(error)
+            });
+        promises.push(uploadPromise)
+    }
+    await Promise.all(promises)
     if (errors.length > 0) {
         throw errors.join(" ");
     }
