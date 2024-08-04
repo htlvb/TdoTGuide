3d499108770941c1bd129a56ab56417a07c63d9d
Johannes Egger <j.egger@posteo.at>
2024-08-04 22:34:36 +0200

    Render description as markdown

--- src/TdoTGuide.Visitor.Client/src/components/ProjectListItem.vue
+++ src/TdoTGuide.Visitor.Client/src/components/ProjectListItem.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { Dto } from '@/Types'
 import ExpandableName from './ExpandableName.vue'
-import { useTourStore } from '@/stores/tour';
+import { useTourStore } from '@/stores/tour'
+import MarkdownIt from 'markdown-it'
 
 const props = defineProps<{
   project: Dto.Project,
@@ -9,6 +10,16 @@ const props = defineProps<{
 }>()
 
 const tourStore = useTourStore()
+
+const md = new MarkdownIt({ linkify: true })
+const defaultRender = md.renderer.rules.link_open || ((tokens, idx, options, env, self) => {
+  return self.renderToken(tokens, idx, options)
+});
+md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+  tokens[idx].attrSet('target', '_blank')
+  return defaultRender(tokens, idx, options, env, self)
+};
+const projectDescription = md.render(props.project.description)
 
 const projectDepartments = props.project.departments
   .map(departmentId => props.departments.find(v => v.id === departmentId))
@@ -21,15 +32,14 @@ const getRegularTimeSelectionText = (intervalMinutes: number) => {
   else if (intervalMinutes % 60 === 0) return `alle ${intervalMinutes / 60} Stunden`
   else `alle ${intervalMinutes} Minuten`
 }
-
 </script>
 
 <template>
   <div class="border rounded p-4 flex flex-col gap-2">
     <div class="flex flex-row">
       <div class="flex flex-col gap-2 grow">
-        <h3>{{ project.title }}</h3>
-        <p>{{ project.description }}</p>
+        <h3 class="text-xl">{{ project.title }}</h3>
+        <p class="description" v-html="projectDescription"></p>
         <div v-if="projectDepartments.length > 0" class="flex flex-wrap gap-2">
           <ExpandableName v-for="department in projectDepartments" :key="department.id" :short-name="department.name" :long-name="department.longName" class="button !text-white" :style="{ 'background-color': department.color}" />
         </div>
@@ -55,3 +65,21 @@ const getRegularTimeSelectionText = (intervalMinutes: number) => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.description {
+    @apply leading-6;
+}
+.description :deep(h1) {
+    @apply text-lg;
+}
+.description :deep(ol) {
+    @apply list-inside list-decimal;
+}
+.description :deep(ul) {
+    @apply list-inside list-disc;
+}
+.description :deep(a) {
+    @apply text-blue-700 dark:text-blue-300 border-blue-700 dark:border-blue-300 cursor-pointer hover:border-b;
+}
+</style>
