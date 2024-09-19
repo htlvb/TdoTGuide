06fee2791b6d7d0b88631a41ca99874740038506
Johannes Egger <j.egger@posteo.at>
2024-09-19 17:26:45 +0200

    Remove unused script

--- watch-tailwind.ps1
+++
@@ -1,5 +0,0 @@
-.\.tools\tailwindcss.exe `
-    --input .\src\TdoTGuide.Admin.Client\wwwroot\css\app.css `
-    --output .\src\TdoTGuide.Admin.Client\wwwroot\css\app.min.css `
-    --watch `
-    --poll
